416ea8ad53dbb03fe5b342be887e55a8392074c3
Wojciech Barczynski <wojciech.barczynski@smacc.io>
2018-01-02 13:24:17 +0100

    Provide a resource for mongodb

--- resource/mongodb.go
+++ resource/mongodb.go
@@ -1,22 +1,55 @@
 package resource
 
 import (
+	"github.com/microdevs/missy/config"
 	"gopkg.in/mgo.v2"
+	"log"
 )
 
-var mgoInstance *mgo.Database
+var mgoInstance *MgoDb
+
+const MgoResourceName = "mongodb"
+const MgoEnvDSN = MgoResourceName + ".dataSourceName"
 
 // Mysql is a type that holds the current active connection and its properties
-type Mgo struct {
-	Username string
-	Password string
-	Db       string
-	Host     string
-	Port     string
-	Session  *mgo.Session
+type MgoDb struct {
+	Dsn     string
+	Session *mgo.Session
 }
 
+func MgoSession() *mgo.Session {
+	if !config.GetInstance().ResourceAvailable(MgoResourceName) {
+		log.Panic("Resource MySQL is not configured. Please add a resource entry in .missy.yml")
+		return nil
+	}
 
-func MgoConnection() *mgo.Database {
+	once.Do(func() {
+		dsn := config.Get(MgoEnvDSN)
+		session, err := mgo.Dial(dsn)
 
+		if err != nil {
+			log.Fatal("Dialing into %s: %v", dsn, err)
+		}
+
+		log.Printf("Connected to DB %s", dsn)
+
+		// Setup adds the MongoDb parameters to the service config
+		mgoInstance = &MgoDb{Dsn: dsn, Session: session}
+	})
+
+	// Do not forget to close it.
+	return mgoInstance.Session.Clone()
 }
+
+func (r *MgoDb) Setup(c *config.Config) {
+	dsn := config.EnvParameter{
+		EnvName:      "MYSQL_DSN",
+		Mandatory:    true,
+		InternalName: MgoEnvDSN,
+		Usage:        MgoResourceName + " Data Service Name",
+		DefaultValue: "",
+	}
+	c.AddEnv(dsn)
+}
+
+// Leave the DB name empty so that we use the DB from the dialed url
