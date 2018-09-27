cc384814a32953288fa046507687820e0cf19ca1
Sven Loth <sven.loth@me.com>
2018-09-27 15:01:26 +0200

    feat(service): Added TLS and token policy helper (#39)
    * feat(service): Added TLS and token policy helper
    
    * feat(service): added more checks
    
    * fix(deps): fixed monkey patch version v1.0.0
    
    * fix(service): typo in log message
    
    * fix(service): typo in log messages and godoc
    
    * feat(service): added client to call other services with custom CA
    
    * feat(service): added tests for http client

--- service/helpers.go
+++ service/helpers.go
@@ -1,7 +1,9 @@
 package service
 
 import (
+	"github.com/microdevs/missy/log"
 	"net/http"
+	"reflect"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/context"
@@ -16,4 +18,49 @@ func Vars(r *http.Request) map[string]string {
 // Token returns the validated auth token from the request context
 func Token(r *http.Request) *jwt.Token {
 	return context.Get(r, "token").(*jwt.Token)
+}
+
+// TokenHasAccess checks if a valid access token contains a given policy in a context
+func TokenHasAccess(r *http.Request, context string, policy string) bool {
+	token := Token(r)
+	// return false if there is no token
+	if token == nil {
+		return false
+	}
+	// let's assume the claims are map claims because this is what our IAM delivers
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		log.Warn("Invalid token format: token claims are not of type jwt.MapClaims")
+		return false
+	}
+	// if the claims do not contain policies return false
+	contextPolicies, ok := claims["policies"].(map[string]interface{})
+	if !ok {
+		log.Warn("Invalid token format: policies inside claims are not of type map[string]interface{}")
+		return false
+	}
+	// if the policies do not contain the context return false
+	if contextPolicies[context] == nil {
+		return false
+	}
+	policies, ok := contextPolicies[context].([]interface{})
+	if !ok {
+		log.Warn("Invalid token format: Policy context value does not match type []interface{}")
+		return false
+	}
+	// look for the policy that matches the requested policy
+	// if found return true
+	for _, p := range policies {
+		ps, ok := p.(string)
+		// if policy cannot asserted to type string we silently skip this entry
+		if !ok {
+			log.Warnf("Invalid token format: Policy is not of type string but %s", reflect.TypeOf(p).String())
+			continue
+		}
+		if ps == policy {
+			return true
+		}
+	}
+	// if the requested policy was not found in the context, the function returns false
+	return false
 }
