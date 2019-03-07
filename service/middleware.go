3fbe843ef92c456a87efbfb918e203b4c6e0cbb0
Mateusz Warzyński <mateusz.warzynski@hypatos.ai>
2019-03-07 18:35:28 +0100

    feat(policies): method for getting policies from token

--- service/middleware.go
+++ service/middleware.go
@@ -5,13 +5,31 @@ import (
 	"net/http"
 )
 
-type PolicyRequestValidator struct {
+const (
+	tokenClaimsPoliciesKey = "policies"
+)
+
+// PolicyManager allows to validate and fetch policies from the HTTP request.
+type PolicyManager struct {
 }
 
-// PolicyValidInRequest validates policy in request
-func (p *PolicyRequestValidator) PolicyValidInRequest(policy string, req *http.Request) error {
+// PolicyValidInRequest validates policy in HTTP request.
+func (p *PolicyManager) PolicyValidInRequest(policy string, req *http.Request) error {
 	if !TokenHasAccess(req, policy) {
 		return fmt.Errorf("token hasn't required policy")
 	}
 	return nil
+}
+
+// TokenPolicies returns all policies from the token in HTTP request.
+func (p *PolicyManager) TokenPolicies(req *http.Request) (map[string]interface{}, bool) {
+	policiesRaw, found := TokenClaims(req)[tokenClaimsPoliciesKey]
+	if !found {
+		return nil, false
+	}
+	policies, ok := policiesRaw.(map[string]interface{})
+	if !ok {
+		return nil, false
+	}
+	return policies, true
 }
